04891a17d6e884a3c9b594c4645ab4b7d7aec07e
liaojiale <k_liaojiale@163.com>
2019-10-17 11:12:46 +0800

    feat:optimize oauth2-php

--- oauth2-php/server.php
+++ oauth2-php/server.php
@@ -1,52 +1,46 @@
-
-
-
-
-
 <?php
-/**
- * @author liaosp.top
- * @Time: 2018/11/23 -16:55
- * @Version 1.0
- * @Describe: 更新用户数据时，把redis 数据更新了
- * 1:
- * 2:
- * ...
- */
-require_once('./oauth2-server-php/src/OAuth2/Autoloader.php');
-$dsn      = 'mysql:dbname=k-oauth2-tp5;host=localhost';
+
+$dsn      = 'mysql:dbname=k-oauth2-tp5;host=localhost'; // my_oauth2_db
 $username = 'root';
 $password = 'root';
 
+// error reporting (this is a demo, after all!)
 ini_set('display_errors',1);error_reporting(E_ALL);
 
+// Autoloading (composer is preferred, but for this example let's just do this)
+require_once('oauth2-server-php/src/OAuth2/Autoloader.php');
 OAuth2\Autoloader::register();
-//如果是redis 把注释去掉，同时把下面的//$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));注释了
-//$predis = new \Predis\Client(['scheme' =>'tcp',' host' =>'localhost','port' =>6379]);
-//$storage = new OAuth2\Storage\Redis($predis);
-//$storage->setClientDetails('testtest', 'testpass', 'http://baidu.com/','client_credentials','trut');
-//如果是密码模式：如果使用其他运行内存则放开，数据库模式不要运行
-// create some users in memory
-//$users = array('bshaffer' => array('password' => 'brent123', 'first_name' => 'Brent', 'last_name' => 'Shaffer'));
-// create a storage object
-//$storage = new OAuth2\Storage\Memory(array('user_credentials' => $users));
-// create the grant type
-//如果用数据库 把注释去掉
+
+// $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
 $storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
-//设置刷新token，同时设置时间过期为28天
-$server = new OAuth2\Server($storage,array(
-    'refresh_token_lifetime'         => 2419200,
-    'access_lifetime'        => 60,//调试开启60秒
-    'allow_implicit' => true
+
+// Pass a storage object or array of storage objects to the OAuth2 server class
+// $server = new OAuth2\Server($storage);
+$server = new OAuth2\Server($storage, array(
+  'always_issue_new_refresh_token' => true,
+  'refresh_token_lifetime'         => 2419200, // 28 days Default: 1209600 (14 days)
 ));
-$grantType = new OAuth2\GrantType\UserCredentials($storage);
-// add the grant type to your OAuth server
-$server->addGrantType($grantType);
-// Add the "Client_Credentials" grant type (it is the simplest of the grant types)
+
+// Add the "Client Credentials" grant type (it is the simplest of the grant types)
 $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+
 // Add the "Authorization Code" grant type (this is where the oauth magic happens)
 $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
 
 // create the grant type
+$grantType = new OAuth2\GrantType\UserCredentials($storage);
+
+// add the grant type to your OAuth server
+$server->addGrantType($grantType);
+
+// create the grant type
 $grantType = new OAuth2\GrantType\RefreshToken($storage);
+
+// the refresh token grant request will have a "refresh_token" field
+// with a new refresh token on each request
+$grantType = new OAuth2\GrantType\RefreshToken($storage, array(
+  'always_issue_new_refresh_token' => true,
+));
+
+// add the grant type to your OAuth server
 $server->addGrantType($grantType);
